ca84773f9c1a1381a60d3e292b776edf506fade3
LAPTOP-T3EIG9TQ\user <ella96815@gmail.com>
2022-08-04 17:45:29 +0800

    做的事: 讓角色射擊(玩家和敵人) 心情:心累 可以改進:多練習文法,英文需加油

--- Jin/config.py
+++
@@ -1,9 +0,0 @@
-import sys
-from os import path
-
-from MyGame.src.MyGame import MyGame
-
-sys.path.append(path.dirname(__file__))
-GAME_SETUP = {
-    "game": MyGame,
-}
---
+++ Racing/src/Bullet.py
@@ -0,0 +1,53 @@
+import pygame
+from mlgame.view.view_model import create_rect_view_data
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, is_player: bool, init_pos: tuple, init_size: tuple, play_rect_area: pygame.Rect):
+        super().__init__()
+        self._play_rect_area = play_rect_area
+        self.rect = pygame.Rect(*init_pos, *init_size)
+        self.is_player = is_player
+        if is_player:
+            self.color = "#FFA500"
+        else:
+            self.color = "#000000"
+
+    def update(self, *args, **kwargs) -> None:
+        if not self.is_player:
+            self.rect.y += 10
+        else:
+            self.rect.y -= 10
+
+        if self.rect.top <= self._play_rect_area.top:
+            is_out = True
+        elif self.rect.bottom >= self._play_rect_area.bottom:
+            is_out = True
+        else:
+            is_out = False
+
+        if is_out:
+            self.kill() #刪除螢幕外的子彈
+
+    @property
+    def xy(self):
+        return self.rect.topleft
+
+    @property
+    def game_object_data(self):
+        return create_rect_view_data(
+            name="Bullet"
+            , x=self.rect.x
+            , y=self.rect.y
+            , width=self.rect.width
+            , height=self.rect.height
+            , color=self.color
+            , angle=0)
+
+
+
+
+
+
+
+
