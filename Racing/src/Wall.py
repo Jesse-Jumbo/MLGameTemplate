762225cb253ede29b2b027ee0e5f6cd05346b61e
DESKTOP-5MGUBVO\snps <ab0970300053@gmail.com>
2022-08-07 22:28:18 +0800

    no message

--- Racing/src/Wall.py
+++ Racing/src/Wall.py
@@ -4,12 +4,15 @@ import pygame
 import random
 from mlgame.view.view_model import create_rect_view_data
 
-peggy = ["#000000","#ffffff","#ff0000","#ffff00","#00ff00","#8c8c8c","#0000ff","#21A1F1","#00FFFF","#FF00FF","#282828","#646464","#643705"]
+peggy = ["#000000", "#ffffff", "#ff0000", "#ffff00", "#00ff00", "#8c8c8c", "#0000ff", "#21A1F1", "#00FFFF", "#FF00FF",
+         "#282828", "#646464", "#643705"]
+
+
 class Wall(pygame.sprite.Sprite):
     def __init__(self, init_pos: tuple, init_size: tuple):
         super().__init__()
         self.rect = pygame.Rect(*init_pos, *init_size)
-        self.color = peggy[random.randint(0,11)]
+        self.color = peggy[random.randint(0, 11)]
 
     @property
     def xy(self):
@@ -25,11 +28,3 @@ class Wall(pygame.sprite.Sprite):
             , height=self.rect.height
             , color=self.color
             , angle=0)
-
-
-
-
-
-
-
-
