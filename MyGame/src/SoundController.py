9a802063665bfbc8ab3951adab3078ba89fcf051
JesseJumbo <jessie97013@gmail.com>
2022-08-11 17:43:50 +0800

    reset orgin_game

---
+++ MyGame/src/SoundController.py
@@ -0,0 +1,13 @@
+import pygame.mixer
+
+
+class SoundController:
+    def play_music(self, music_path: str, volume: float) -> None:
+        pygame.mixer.init()
+        pygame.mixer.music.load(music_path)
+        pygame.mixer.music.set_volume(volume)
+        pygame.mixer.music.play(-1)
+
+    def play_sound(self, music_path: str, volume: float, maz_time: int) -> None:
+        pygame.mixer.init()
+        pygame.mixer.Sound(music_path).play(maxtime=maz_time).set_volume(volume)
